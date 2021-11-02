cac44e262b74cdb25a1ad5d609ae5ac486ff1559
relaxcn <956902354@qq.com>
2021-11-02 23:48:54 +0800

    11章结束

---
+++ 11ch/11_38_2.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include<unordered_map>
+
+using namespace std;
+
+unordered_map<string, string> buildMap(ifstream &map_file);
+void word_transform(ifstream &, ifstream &);
+const string & transform(const string &, const unordered_map<string, string> &);
+
+int main()
+{
+    ifstream map_file("./map_file.txt");
+    ifstream input("./input.txt");
+    word_transform(map_file, input);
+    return 0;
+}
+
+void
+word_transform(ifstream &map_file, ifstream &input) 
+{
+    auto strans_map = buildMap(map_file);
+    string text;
+    while (getline(input, text)) {
+        istringstream stream(text);
+        string word;
+        bool firstword = true;
+        while (stream >> word) {
+            if (firstword)
+                firstword = false;
+            else
+                cout << " ";
+            cout << transform(word, strans_map);
+        }
+        cout << endl;
+    }
+}
+
+unordered_map<string, string>
+buildMap(ifstream &map_file)
+{
+    unordered_map<string, string> trans_map;
+    string key;
+    string value;
+
+    while (map_file >> key && getline(map_file, value)) {
+        if (value.size() > 1)
+            trans_map[key] = value.substr(1);
+        else
+            throw runtime_error("no rule for " + key);
+    }
+    return trans_map;
+}
+
+const string &
+transform(const string &s, const unordered_map<string, string> &m)
+{
+    // find 返回一个指向查找元素的迭代器
+    auto map_it = m.find(s);
+    // if find
+    if (map_it != m.cend()) {
+        return map_it->second;
+    } else {
+        return s;
+    }
+}
