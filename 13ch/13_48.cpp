22dea96b9956d1946f51b8c352747827681e6825
relaxcn <956902354@qq.com>
2021-11-13 19:41:24 +0800

    13.6.1

---
+++ 13ch/13_48.cpp
@@ -0,0 +1,14 @@
+#include"13_44_String.hpp"
+#include<vector>
+#include<iostream>
+
+int main()
+{
+    std::vector<String> vec;
+    vec.push_back("hello");
+    std::cout << "-----------" << std::endl;
+    vec.push_back("world");
+    std::cout << "-----------" << std::endl;
+    vec.push_back("ciel");
+    std::cout << "-----------" << std::endl; 
+}
